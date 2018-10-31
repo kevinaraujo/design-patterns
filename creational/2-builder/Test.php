88abe1350ad60208a169041fb8e66dc889dc2f96
Kevin Araujo <kevin.araujo@mobly.com.br>
2018-10-31 10:53:20 -0300

    1.2 -

--- creational/2-builder/Test.php
+++ creational/2-builder/Test.php
@@ -1,1 +1,21 @@
-<?phpnamespace DesignPatterns\Creational\Builder;require_once('CarBuilder.php');require_once('Director.php');class Test{    public function testCanBuildCar()    {        $carBuilder = new CarBuilder();        $newVehicle = (new Director())->build($carBuilder);        var_dump($newVehicle);    }}$test = new Test();$test->testCanBuildCar();
+<?php
+
+namespace DesignPatterns\Creational\Builder;
+
+require_once('CarBuilder.php');
+require_once('Director.php');
+
+class Test
+{
+    public function testCanBuildCar()
+    {
+        $carBuilder = new CarBuilder();
+        $newVehicle = (new Director())->build($carBuilder);
+
+        var_dump($newVehicle);
+    }
+}
+
+
+$test = new Test();
+$test->testCanBuildCar();
